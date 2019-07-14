d1358eda368ca8c091ddd6c965addf6e9498671b
JiaHao-Chu <JiaHao.Chu@ibm.com>
2019-07-14 22:03:58 +0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// For Ng material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// For customize import ng module
+import { NgmatModule } from './ngmat/ngmat.module';
+// import { MatButtonModule, MatCheckboxModule, MatToolbarModule } from '@angular/material';
+
+import { CdkmoduleModule } from './cdkmodule/cdkmodule.module';
+import { TodoComponent } from './todo/todo.component';
+@NgModule({
+  declarations: [AppComponent, TodoComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, NgmatModule, CdkmoduleModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/ngmat/ngmat.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatProgressSpinnerModule,
+  MatCardModule,
+  MatMenuModule,
+  MatIconModule
+} from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule
+  ]
+})
+export class NgmatModule {}
